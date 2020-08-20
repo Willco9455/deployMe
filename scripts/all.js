bd327f28356c9f4a77ca08314f72ba7576a6b2ed
will <willmoran6969@gmail.com>
2020-08-20 11:25:27 +0100

    wabang

--- scripts/all.js
+++
@@ -1,7 +0,0 @@
-//button: width-89px
-document.getElementById("title").style="padding-right:89px;";
-var testGlobal = "var was global";
-{
-    let testGlobal = "var was not global";
-    document.write(testGlobal);
-}
